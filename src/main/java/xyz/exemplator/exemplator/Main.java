77dbe1bd4cbd540ce83ccd5fcff5ed0d4d6b564b
Julian Brendl <julianbrendl@gmail.com>
2016-06-11 15:15:27 +0200

    first push

--- src/main/java/xyz/exemplator/exemplator/Main.java
+++ src/main/java/xyz/exemplator/exemplator/Main.java
@@ -1,8 +1,14 @@
 package xyz.exemplator.exemplator;
 
+import xyz.exemplator.exemplator.data.SnippetFetcher;
+
 /**
  * @author LeanderK
  * @version 1.0
  */
 public class Main {
+    public static void main(String[] args) {
+        SnippetFetcher snippetFetcher = new SnippetFetcher();
+        snippetFetcher.fetch("");
+    }
 }
