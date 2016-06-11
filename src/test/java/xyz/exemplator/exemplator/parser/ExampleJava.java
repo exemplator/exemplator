16f48138a19f850ebd5026fd58e218defcd22a3c
LeanderK <Leander.Kurscheidt@gmx.de>
2016-06-11 21:15:05 +0200

    tests: added sandbox + example class

---
+++ src/test/java/xyz/exemplator/exemplator/parser/ExampleJava.java
@@ -0,0 +1,26 @@
+package xyz.exemplator.exemplator.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+/**
+ * @author LeanderK
+ * @version 1.0
+ */
+public class ExampleJava {
+    private final ArrayList<List> test = new ArrayList<>();
+    private final int x = 3;
+    private List<String> test2;
+    private java.util.Set<String> test3;
+    public void test() {
+        List<String> list = new ArrayList<>();
+        list.add("haha");
+        test2();
+        System.out.println(list);
+    }
+
+    public void test2() {
+
+    }
+}
