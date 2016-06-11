96028acbcab512cc17ca36359b00c263109ee14b
LeanderK <Leander.Kurscheidt@gmx.de>
2016-06-11 23:39:45 +0200

    router: updated to new api

--- src/main/java/xyz/exemplator/exemplator/Router.java
+++ src/main/java/xyz/exemplator/exemplator/Router.java
@@ -1,6 +1,8 @@
 package xyz.exemplator.exemplator;
 
+import io.netty.util.concurrent.PromiseCombiner;
 import ratpack.exec.Blocking;
+import ratpack.exec.Promise;
 import ratpack.jackson.Jackson;
 import ratpack.server.RatpackServer;
 import ratpack.server.ServerConfig;
@@ -9,10 +11,14 @@ import xyz.exemplator.exemplator.data.ICodeSearch;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.sun.javafx.tools.resource.DeployResource.Type.data;
+import static java.util.stream.Collectors.toList;
 import static ratpack.jackson.Jackson.fromJson;
 import static ratpack.jackson.Jackson.json;
 import static ratpack.jackson.Jackson.jsonNode;
@@ -24,6 +30,7 @@ import static ratpack.jackson.Jackson.jsonNode;
 public class Router {
     private final int port;
     private final ICodeSearch codeSearch;
+    private ExecutorService executorService = Executors.newCachedThreadPool();
 
     public Router(int port, ICodeSearch codeSearch) {
         this.port = port;
@@ -58,18 +65,29 @@ public class Router {
                                         List<String> searchTerms = Stream.of(request.getClassName(), request.getMethodName(), request.getPackageName(), request.getToken())
                                                 .filter(Optional::isPresent)
                                                 .map(Optional::get)
-                                                .collect(Collectors.toList());
+                                                .collect(toList());
                                         return Blocking.get(() -> codeSearch.fetch(searchTerms, request.getPage()));
                                     })
-                                    .map(list -> list.stream()
-                                                .map(sample -> {
-                                                    Response.Position position = new Response.Position(1, 1);
-                                                    List<Response.Position> positions = new ArrayList<>();
-                                                    positions.add(position);
-                                                    return new Response.Occurrence(sample.getUrl(), "", "", positions);
-                                                })
-                                                .collect(Collectors.toList())
+                                    .map(codeSamples -> codeSamples.stream()
+                                            .map(sample -> CompletableFuture.supplyAsync(() -> codeSearch.rawFetch(sample), executorService)
+                                                    .thenApply(result -> {
+                                                        Response.Position position = new Response.Position(1, 1);
+                                                        List<Response.Position> positions = new ArrayList<>();
+                                                        positions.add(position);
+                                                        return new Response.Occurrence(result.getRawUrl(), result.getUserUrl(), result.getCodeSnippet(), positions);
+                                                    })
+                                            )
+                                            .collect(Collectors.toList())
                                     )
+                                    .map(futures ->
+                                            CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
+                                                    .thenApply(v -> futures.stream()
+                                                            .map(CompletableFuture::join)
+                                                            .collect(toList())
+                                                    )
+                                    )
+                                    .flatMap((ratpack.func.Function<CompletableFuture<List<Response.Occurrence>>, Promise<List<Response.Occurrence>>>)future ->
+                                            Promise.async(downstream -> downstream.accept(future)))
                                     .map(Response::new)
                                     .map(Jackson::json));
                         })
