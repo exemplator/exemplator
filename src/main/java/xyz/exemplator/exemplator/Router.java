7a2b4d36ea9bd2f6bcdc88dc4c4430b49b06af76
LeanderK <Leander.Kurscheidt@gmx.de>
2016-06-11 14:34:24 +0200

    router: created router

---
+++ src/main/java/xyz/exemplator/exemplator/Router.java
@@ -0,0 +1,12 @@
+package xyz.exemplator.exemplator;
+
+/**
+ * @author LeanderK
+ * @version 1.0
+ */
+public class Router {
+    public void init() {
+        RatpackServer.start(server -> server
+        )
+    }
+}
